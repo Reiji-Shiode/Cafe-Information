72c767f4990ced62fc463484ae610637ef64f37f
Reiji-Shiode <rerere0606@gmail.com>
2024-05-22 13:48:17 +0900

    プロジェクトの作成。

---
+++ src/main/java/com/raisetech/cafeinfo/CafeinfoApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech.cafeinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech/cafeinfo/CafeinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech.cafeinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
