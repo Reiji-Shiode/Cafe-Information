5974c77728c9ce1a113f2dfbbe788ddbea581f6f
Reiji-Shiode <rerere0606@gmail.com>
2024-05-22 15:03:56 +0900

    DB周りの環境構築 README.mdの修正

---
+++ sql/001-create-table-and-load-data.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS cafes;
+
+CREATE TABLE cafes (
+  id int unsigned AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  place VARCHAR(40) NOT NULL,
+  regular_holiday VARCHAR(50) NOT NULL,
+  opening_hour VARCHAR(50) NOT NULL,
+  number_of_seat int NOT NULL,
+  birthplace VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+INSERT INTO cafes (name, place, regular_holiday, opening_hour, number_of_seat, birthplace) VALUES ("Starbucks Reserve Roastery", "中目黒", "年中無休", "7時-22時", 300, "シアトル");
+INSERT INTO cafes (name, place, regular_holiday, opening_hour, number_of_seat, birthplace) VALUES ("nadoya no katte", "代々木上原", "月、火、水、木", "9時-18時", 15, "代々木上原");
+INSERT INTO cafes (name, place, regular_holiday, opening_hour, number_of_seat, birthplace) VALUES ("Streamer Coffee Company", "原宿", "年中無休","8時-20時", 56, "原宿");
+INSERT INTO cafes (name, place, regular_holiday, opening_hour, number_of_seat, birthplace) VALUES ("ブルーボトルコーヒー清澄白河フラッグシップカフェ", "清澄白河", "年中無休", "8時-19時", 47, "カリフォルニア");
+INSERT INTO cafes (name, place, regular_holiday, opening_hour, number_of_seat, birthplace) VALUES ("ARC", "蔵前", "年中無休", "平日10時-23時 休日8時半-23時", 52, "蔵前");
+INSERT INTO cafes (name, place, regular_holiday, opening_hour, number_of_seat, birthplace) VALUES ("T.Y.HARBOR", "天王洲アイル", "年中無休", "11時半-22時", 350, "天王洲アイル");
+INSERT INTO cafes (name, place, regular_holiday, opening_hour, number_of_seat, birthplace) VALUES ("No.13 Cafe", "新宿", "日", "11時-16時半", 30, "新宿");
