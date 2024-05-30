9e56eeadee9b3f268417cae36173506bc530118e
Reiji-Shiode <rerere0606@gmail.com>
2024-05-30 22:34:58 +0900

    CafeMapperの修正 DBをキャメルケースからスネークケースへ変更

--- src/main/java/com/raisetech/cafeinfo/CafeMapper.java
+++ src/main/java/com/raisetech/cafeinfo/CafeMapper.java
@@ -20,7 +20,7 @@ public interface CafeMapper {
     @Select("SELECT * FROM cafes WHERE id =#{id}")
     Optional<Cafe> findById(int id);
 
-    @Insert("INSERT INTO cafes (name, place, regularHoliday, openingHour, numberOfSeat, birthplace) VALUES (#{name}, #{place}, #{regularHoliday}, #{openingHour}, #{numberOfSeat}, #{birthplace})")
+    @Insert("INSERT INTO cafes (name, place, regular_holiday, opening_hour, number_of_seat, birthplace) VALUES (#{name}, #{place}, #{regularHoliday}, #{openingHour}, #{numberOfSeat}, #{birthplace})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void insert(Cafe cafe);
 }
